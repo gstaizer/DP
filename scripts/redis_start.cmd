1043908891f59bf3c88d850a5081fbc5b6b6cd8c
NextGenen <32591950+gstaizer@users.noreply.github.com>
2021-10-25 11:39:09 +0300

    upd

--- scripts/redis_start.cmd
+++ scripts/redis_start.cmd
@@ -3,7 +3,7 @@ setx DB_EU "localhost:6001"
 setx DB_OTHER "localhost:6002"
 
 cd ..\..\Redis\
-start "DB_KEYMAP" redis-server
+start "DB_BASIC" redis-server
 start "DB_RUS" redis-server --port 6000
 start "DB_EU" redis-server --port 6001
 start "DB_OTHER" redis-server --port 6002
